fa5591297339f4e887fc30faf459e1dde81e4b84
trumbladome <112825532+trumbladome@users.noreply.github.com>
2025-08-27 19:18:59 +0100

    Add files via upload

---
+++ ImageAssistant-Batch-Image-Downloader-Chrome-Web-Store/scripts/favorite.js
@@ -0,0 +1,7 @@
+/**
+ * ImageAssistant
+ * Project Home: http://www.pullywood.com/ImageAssistant/
+ * Author: Joey
+ * Copyright (C) 2013-2024 普利坞(Pullywood.com)
+**/
+"use strict";window.location.href="https://www.pullywood.com/ImageAssistant/favorite.html";
